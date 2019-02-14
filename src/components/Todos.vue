b4d58caba90f974a4e474aca9ad8955fe47aa854
Randy Harkediansa <randy.harkediansa@gmail.com>
2019-02-14 11:24:06 +0800

    Skeleton : Simple to do list
    Very simple to do list using vuejs

---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="todo-container">
+        <h1> Move forward today! </h1>
+        <div v-bind:key="list.id" v-for="list in todos">
+            <Todoitem v-bind:todo="list" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Todoitem from './Todoitem.vue';
+
+export default {
+    name: "Todos",
+    components: {
+        Todoitem
+    },
+    props: ["todos"]
+}
+</script>
+
+<style scoped>
+    .todo-container > h1 {
+        margin: 10px 0;
+    }
+</style>
